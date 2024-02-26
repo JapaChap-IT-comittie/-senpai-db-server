f55454754de61be2cc9b23bb7542b20805c300ae
ask-t <asktakahashi@go.byuh.edu>
2024-02-25 15:13:24 -1000

    add redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Redirect('http://localhost:3000/api', 301)
+  // Fix for Problem 1 and Problem 2
+  handleGetRequest(): void {}
 }
