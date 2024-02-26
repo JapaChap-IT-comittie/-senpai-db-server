370bbb267c1da00edc3a98b76e404d70bf75bd4e
ask-t <asktakahashi@go.byuh.edu>
2024-02-25 15:39:28 -1000

    change description

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Senpai DB')
-    .setDescription('The cats API description')
+    .setDescription('')
     .setVersion('1.0')
     .addTag('')
     .build();
