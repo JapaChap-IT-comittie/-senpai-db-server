8dc8dd7e38065c6cc5678fb4ab344369996f467e
ask-t <asktakahashi@go.byuh.edu>
2024-02-25 19:35:29 -1000

    change path

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/user/user.schema';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
 
 @Module({
   imports: [
